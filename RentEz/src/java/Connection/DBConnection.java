a62bf41a90a1435d7cb3dfee3b666ccb66ef5b17
DatNguyenTran <dattnde180870@fpt.edu.vn>
2025-06-15 18:38:34 +0700

    UC-18-RentalBooking

--- RentEz/src/java/Connection/DBConnection.java
+++ RentEz/src/java/Connection/DBConnection.java
@@ -6,7 +6,7 @@ public class DBConnection {
     private static String driverClass = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
     private static String username = "sa";
     private static String password = "123";
-    private static String jdbcURL = "jdbc:sqlserver://DESKTOP-B30H0L1;databaseName=RentEZ;encrypt=true;trustServerCertificate=true;loginTimeout=30";
+    private static String jdbcURL = "jdbc:sqlserver://DATTRAN;databaseName=RentEZ;encrypt=true;trustServerCertificate=true;loginTimeout=30";
     //kết nối datebase dùng URL username, password. Thả ra lỗi 
     
     public static Connection getConnection(){
